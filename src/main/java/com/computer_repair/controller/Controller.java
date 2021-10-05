cf48a3bcdf8255259251763aaa89a05b17cada87
fan <664130988@qq.com>
2021-10-06 03:04:14 +0800

    初始化

---
+++ src/main/java/com/computer_repair/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.computer_repair.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/getTest")
+    @ResponseBody
+    public String getTest() {
+        return "ok！";
+    }
+}
---
+++ src/test/java/com/computer_repair/controller/ControllerTest.java
@@ -0,0 +1,13 @@
+package com.computer_repair.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void getTest() {
+        System.out.println("=============");
+    }
+}
