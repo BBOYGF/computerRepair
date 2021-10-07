4b895b21ead73db4409906f26fb08d4053a7d29b
fan <664130988@qq.com>
2021-10-07 16:25:19 +0800

    测试3

--- src/main/java/com/computer_repair/controller/Controller.java
+++ src/main/java/com/computer_repair/controller/Controller.java
@@ -1,10 +1,11 @@
 package com.computer_repair.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller
-public class Controller {
+class Controller {
     @GetMapping("/getTest")
     @ResponseBody
     public String getTest() {
